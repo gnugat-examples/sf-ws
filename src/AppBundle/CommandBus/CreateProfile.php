dac875b541c512613e12a335302a009374a58e74
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-03-02 19:34:40 +0000

    Added check on name presence in the request

--- src/AppBundle/CommandBus/CreateProfile.php
+++ src/AppBundle/CommandBus/CreateProfile.php
@@ -11,6 +11,9 @@ class CreateProfile implements Message
 
     public function __construct($name)
     {
+        if (null === $name) {
+            throw new \DomainException('The "name" parameter is missing from the request\'s body');
+        }
         $this->name = $name;
     }
 }
